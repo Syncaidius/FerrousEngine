9397785f8b32bcc17cdf3cddaec994654cf280e8
James Yarwood <syncaidius@googlemail.com>
2019-10-05 23:04:04 +0100

    Expanded implementation of List<T>

--- FerrousEngine/collection.h
+++ FerrousEngine/collection.h
@@ -24,13 +24,15 @@ namespace fe {
 			virtual BaseIterator<T> end() = 0;
 
 			virtual void clear() = 0;
-			virtual bool isEmpty() = 0;
 
 			virtual bool contains(T item) = 0;
 			virtual bool add(T item) = 0;
 			virtual bool remove(T item) = 0;
 
+			virtual void copyTo(T* dest) = 0;
+
 			inline uint32_t count() const { return _count; }
+			inline bool isEmpty() const { return _count == 0; }
 
 		protected:
 			uint32_t _count;
--- FerrousEngine/list.h
+++ FerrousEngine/list.h
@@ -6,16 +6,23 @@
 namespace fe {
 	namespace collections {
 		template<typename T>
-		class list : public Collection<T> {
+		class List : public Collection<T> {
 
 		public:
 			class iterator : public BaseIterator<T> {
 				// TODO implement
 			};
 
-			inline list(int initialCapacity, FerrousAllocator* allocator = Memory::get()) {
+			inline List(uint32_t initialCapacity = 1, FerrousAllocator* allocator = Memory::get()) {
+				assert(initialCapacity > 0);
+
 				_allocator = allocator;
+				_capacity = initialCapacity;
+				_first = _allocator->allocType<T>(_capacity);
 			}
+
+			List<T>& operator=(const List<T>&) = delete;
+			List<T> operator=(const List<T>&) = delete;
 
 			iterator begin() override {
 				return iterator(this, 0);
@@ -26,11 +33,38 @@ namespace fe {
 			}
 
 			inline bool add(T item) override {
-				// TODO implement
+				if (_count == _capacity) {
+					_capacity *= 2;
+
+					// Copy existing elements to new location.
+					T* newMem = _allocator->allocType<T>(_capacity);
+					Memory::copyType(newMem, _first, _count);
+					_allocator->dealloc(_first);
+					_first = newMem;
+				}
+
+				_first[_count++] = item;
+				return true;
 			}
 
-			inline bool addCollection(Collection<T> collection) {
-				// TODO implement
+			inline void addCollection(Collection<T>& collection) {
+				int required = _count + collection.count();
+
+				if (required > _capacity) {
+					_capacity *= 2;
+
+					// Add whatever we're short by, to the new capacity.
+					if (_capacity < required)
+						_capacity += required - _capacity;
+
+					T* newMem = _allocator->allocType<T>(_capacity);
+					Memory::copyType(newMem, _first, _count);
+					_allocator->dealloc(_first);
+					_first = newMem;
+				}
+
+				// Copy items from collection to the end of the current list.
+				collection.copyTo(&_first[_count]);
 			}
 
 			inline bool remove(T item) override {
@@ -53,7 +87,10 @@ namespace fe {
 					Memory::copyType(&_first[index], _first[index + 1], (_count - (index + 1)));
 
 				_count--;
-				return true;
+			}
+
+			inline void copyTo(T* dest) override {
+				Memory::copyType(dest, _first, _count);
 			}
 
 			inline bool contains(T item) {
@@ -65,6 +102,18 @@ namespace fe {
 				return false;
 			}
 
+			// Removes all empty capacity from the end of the list, if any.
+			inline void trim() {
+				if (_capacity == _count)
+					return 0;
+
+				T * trimmedMem = _allocator->allocType<T>(_count);
+				Memory::copyType(trimmedMem, _first, _count);
+				_allocator->dealloc(_first);
+				_first = trimmedMem;
+				_capacity = _count;
+			}
+
 			inline void clear() override {
 				_count = 0;
 			}
@@ -72,6 +121,10 @@ namespace fe {
 			inline bool isEmpty() override {
 				return _count == 0;
 			}
+
+			inline uint32_t capacity() {
+				return _capacity;
+			}			
 
 			inline T& operator[] (uint32_t index) {
 				if (index >= _count || index < 0)
@@ -82,7 +135,6 @@ namespace fe {
 
 		private:
 			T* _first;
-			T* _end;
 			uint32_t _capacity;
 			FerrousAllocator* _allocator;
 		};
